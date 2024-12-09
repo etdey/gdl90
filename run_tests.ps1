1745711186cfd1f18ce3fd72505445c482671392
Eric Dey <eric@deys.org>
2024-12-09 17:31:11 -0600

    Added PowerShell script for running all unit tests

---
+++ run_tests.ps1
@@ -0,0 +1,31 @@
+#
+# Run all Python unit tests
+#
+
+# Change the cwd to where this script lives
+Push-Location $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+# Test for python
+$PYTHON = Get-Command -ErrorAction SilentlyContinue "python"
+If (-not $PYTHON) {
+    Write-Host "Cannot find an appropriate Python version"
+    Pop-Location
+    Exit 1
+} Else {
+    $py_ver = & $PYTHON.Source "-V"
+    Write-Host "Using $($py_ver): $($PYTHON.Source)"
+}
+
+# Check for the coverage package
+$null = & $PYTHON.Source -m coverage 2>$null
+If ($LASTEXITCODE -eq 0) {
+    # Run tests with code coverage
+    Write-Host "To see code coverage, run: $($PYTHON.Name) -m coverage report"
+    & $PYTHON.Source -m coverage run -m unittest discover -v -p 'test_*.py'
+} Else {
+    # Run tests w/o code coverage
+    & $PYTHON.Source -m unittest discover -v -p 'test_*.py'
+}
+
+# Restore original cwd
+Pop-Location
